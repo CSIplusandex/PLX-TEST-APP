ad495bcddc524e7b8f45130acb7b014fc199f157
CSIplusandex <csi.plusandex@gmail.com>
2024-06-19 16:12:53 -0400

    Estilo CSS

--- resources/views/connect/login.blade.php
+++ resources/views/connect/login.blade.php
@@ -1,25 +1,16 @@
 @extends('connect.master')
 @section('content_connect')
 
-
 <div class="wrapper" style="display: flex; flex-wrap: wrap;">
 	<div class="box">
 		<div class="in">
-			<div class="logo">
-				<img src="{{ url('/static/images/logo_64.png') }}">
+			<div class="logo container">
+				<img src="{{ url('/static/images/logo_64.png') }}" height="100">
 			</div>
 		</div>
 	</div>
-	<div class="container"style="color: red;text-align: center;margin: 20px;width: auto;">
-		<p>¡¡welcome</p>
-	</div>
-	<div class="container"style="color: red;text-align: center;margin: 20px;width: auto;">
-		<p>to</p>
-	</div>
-	<div class="container"style="color: blue;text-align: center;margin: 20px;width: auto;">
-		<p>{{ config('cms.app_name')}}!!</p>
-	</div>
 </div>
+<div class="container"><p>¡¡Bienvenido a {{ config('cms.app_name')}}!!</p></div>
 <div class="form mtop16">
 	{!! Form::open(array('url' => '/', 'id' => 'form_connect_login')) !!}
 	{!! Form::hidden('autocomplete', null, ['class' => 'autocomplete']) !!}
@@ -27,7 +18,7 @@
 	{!! Form::text('email', null, ['class' => 'disableac']) !!}
 	<label for="email">Contraseña</label>
 	{!! Form::password('password', null, ['class' => 'disableac']) !!}
-	{!! Form::submit('ingresar', ['class' => 'mtop16']) !!}
+	{!! Form::submit('Ingresar', ['class' => 'mtop16']) !!}
 	{!! Form::close() !!}
 </div>
 @endsection
--- resources/views/connect/master.blade.php
+++ resources/views/connect/master.blade.php
@@ -4,7 +4,7 @@
 	<div class="connect">
 		<div class="col">
 			<div class="cover">
-				<img width='200' src="{{ url('/static/images/connect_splash.jpg') }}" alt="Un gato, no tengo la otra imagen">	
+				<img height="100"src="{{ url('/static/images/connect_splash.jpg') }}" alt="Un gato, no tengo la otra imagen">	
 			</div>
 		</div>
 		<div class="col">
